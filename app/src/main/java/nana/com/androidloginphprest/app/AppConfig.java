b0ffa907eeab4ee5043056bf241266efb3b58fee
youngstruggle <febrian.nana@gmail.com>
2018-02-22 23:16:26 +0700

    Initial commit

---
+++ app/src/main/java/nana/com/androidloginphprest/app/AppConfig.java
@@ -0,0 +1,15 @@
+package nana.com.androidloginphprest.app;
+
+/**
+ * Created by Nana Febriana on 22-Feb-18.
+ */
+
+public class AppConfig {
+
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.21:10080/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.21:10080/android_login_api/register.php";
+}
+
